a615fad8af79a6433f969ebe8545bbaa87f57d0d
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-05-21 22:24:34 +0000

    feat: add errorHandler function and internal exceptions for server errors

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,49 +1,53 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
 import { prismaClient } from "..";
-import {hashSync, compareSync} from 'bcrypt';
-import * as jwt  from 'jsonwebtoken'
+import { hashSync, compareSync } from "bcrypt";
+import * as jwt from "jsonwebtoken";
 import { JWT_SECRET_KEY } from "../secrets";
 import { BadRequestsException } from "../exceptions/bad-requests";
 import { ErrorCode } from "../exceptions/root";
 import { UnprocessableEntity } from "../exceptions/validation";
 import { SignupSchema } from "../schema/users";
 
-export const signup = async(req: Request , res: Response, next: NextFunction) => {
-    try {
-        SignupSchema.parse(req.body);
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  SignupSchema.parse(req.body);
 
-        const {email, password, name} = req.body;
-    
-        let user = await prismaClient.user.findFirst({where: {email}});
-    
-        if(user) next(new BadRequestsException('User already exists!', ErrorCode.USER_ALREADY_EXISTS));
-    
-        user = await prismaClient.user.create({
-            data: {
-                name,
-                email,
-                password: hashSync(password, 10)
-            }
-        })
-        res.json(user);
-    } catch (error:any) {
-        next(new UnprocessableEntity(error?.issues, 'Unprocessable entity' ,ErrorCode.UNPROCESSABLE_ENTITY))
-    }
+  const { email, password, name } = req.body;
 
-    
-}
-export const login = async(req: Request , res: Response) => {
-    const {email, password} = req.body;
+  let user = await prismaClient.user.findFirst({ where: { email } });
 
-    let user = await prismaClient.user.findFirst({where: {email}});
+  if (user)
+    next(
+      new BadRequestsException(
+        "User already exists!",
+        ErrorCode.USER_ALREADY_EXISTS
+      )
+    );
 
-    if(!user) throw Error('User does not exist!');
+  user = await prismaClient.user.create({
+    data: {
+      name,
+      email,
+      password: hashSync(password, 10),
+    },
+  });
+  res.json(user);
+};
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-    if(!compareSync(password, user.password)) {
-        throw Error("Incorrect password!");
-    }
+  let user = await prismaClient.user.findFirst({ where: { email } });
 
-    const accessToken = jwt.sign({userId: user.id}, JWT_SECRET_KEY)
-    
-    res.json({user,accessToken});
-}
+  if (!user) throw Error("User does not exist!");
+
+  if (!compareSync(password, user.password)) {
+    throw Error("Incorrect password!");
+  }
+
+  const accessToken = jwt.sign({ userId: user.id }, JWT_SECRET_KEY);
+
+  res.json({ user, accessToken });
+};
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { login, signup } from "../controllers/auth";
+import { errorHandler } from "../errorHandler";
 
 const authRoute:Router = Router();
 
-authRoute.post('/signup', signup);
+authRoute.post('/signup', errorHandler(signup));
 
-authRoute.post('/login', login);
+authRoute.post('/login', errorHandler(login));
 
 export default authRoute;
