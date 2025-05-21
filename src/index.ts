274f157d2e5b3bec40e7da961f1f62d7d86cee26
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-05-21 20:32:45 +0000

    error middleware created

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,20 @@ import express, { Express, Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
 import { PrismaClient } from "./generated/prisma";
+import { errorMiddleware } from "./middlewares/errors";
 
 const app: Express = express();
 
 app.use(express.json());
+
+app.use(errorMiddleware);
 
 app.use('/api',rootRouter);
 
 export const prismaClient = new PrismaClient({
   log: ['query']
 })
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log("App working!"));
