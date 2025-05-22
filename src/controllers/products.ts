44a23afdf952ef770b62588b6de82d78554831c1
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-05-22 20:08:03 +0000

    feat: implemented CRUD controllers for products endpoints

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { prismaClient } from "..";
 import { createProductsSchema } from "../schema/products";
+import { NotFoundException } from "../exceptions/notFound";
+import { ErrorCode } from "../exceptions/root";
 
 
 export const createProduct = async(req:Request,res:Response) => {
@@ -22,17 +24,71 @@ export const createProduct = async(req:Request,res:Response) => {
 }
 
 export const updateProduct = async(req:Request, res: Response) => {
+    try {
+        const product = req.body;
 
+        if(product.tags){
+            product.tags = product.tags.join(',')
+        }
+
+        const updatedProduct = await prismaClient.product.update({
+            where: {
+                id: +req.params.id
+            },
+            data: product
+        })
+
+        res.json(updatedProduct);
+        
+    } catch (error) {
+        throw new NotFoundException('Product not found.', ErrorCode.PRODUCT_NOT_FOUND);
+    }
 }
 
 export const deleteProduct = async(req:Request, res:Response) => {
+    const product = await prismaClient.product.delete({
+        where: {
+            id: +req.params.id
+        }
+    })
+
+    res.json('Product successfully deleted.')
 
 }
 
 export const listProducts = async(req:Request, res:Response) => {
 
+    // {
+    //     count: 100,
+    //     data: []
+    // }
+
+    // First get the count
+    const count = await prismaClient.product.count();
+
+    const products = await prismaClient.product.findMany({
+        skip: +req.query.skip! || 0,
+        take: 5
+    })
+
+    res.json({
+        count, data: products
+    })
+
 }
 
 export const getProductById = async(req:Request, res:Response) => {
-    
+
+    try {
+        const product = await prismaClient.product.findFirstOrThrow({
+            where: {
+                id: +req.params.id
+            }
+        })
+        res.json(product);
+        
+    } catch (error) {
+        throw new NotFoundException('Product not found.', ErrorCode.PRODUCT_NOT_FOUND);
+    }
+
 }
