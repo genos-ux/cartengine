dcff5e9e09548d73aa450ced8fb1ce89c44828fc
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-06-10 01:26:25 +0000

    created the dist

--- src/schema/cart.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateCartSchema = void 0;
-const zod_1 = require("zod");
-exports.CreateCartSchema = zod_1.z.object({
-    quantity: zod_1.z.number()
-});
